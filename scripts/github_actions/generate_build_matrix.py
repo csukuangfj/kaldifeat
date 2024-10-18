176222fd649f6db8cb9d76b2fba316688bdeb587
Fangjun Kuang <csukuangfj@gmail.com>
2024-10-18 10:38:24 +0800

    update upload artifact version

--- scripts/github_actions/generate_build_matrix.py
+++ scripts/github_actions/generate_build_matrix.py
@@ -260,7 +260,7 @@ def generate_build_matrix(enable_cuda, for_windows, for_macos, test_only_latest_
             ),
         },
         "2.5.0": {
-            # Only Linux support python 3.13
+            # Only Linux supports python 3.13
             "python-version": ["3.9", "3.10", "3.11", "3.12", "3.13"],
             "cuda": (
                 ["11.8", "12.1", "12.4"]  # default 12.4
