bb9aad7dbcf59921aa9768df7f9bc6ae822c5e6e
Fangjun Kuang <csukuangfj@gmail.com>
2026-06-26 09:37:08 +0800

    Fix for building

--- scripts/github_actions/build-ubuntu-cpu-arm64.sh
+++ scripts/github_actions/build-ubuntu-cpu-arm64.sh
@@ -28,7 +28,8 @@ python3 -m pip install wheel twine typing_extensions
 python3 -m pip install bs4 requests tqdm auditwheel patchelf
 
 echo "Installing torch"
-python3 -m pip install -qq torch==$TORCH_VERSION || \
+python3 -m pip install -qq torch==$TORCH_VERSION+cpu -f https://download.pytorch.org/whl/torch_stable.html || \
+python3 -m pip install -qq torch==$TORCH_VERSION+cpu -f https://download.pytorch.org/whl/torch/ || \
 python3 -m pip install -qq torch==$TORCH_VERSION -f https://download.pytorch.org/whl/torch_stable.html || \
 python3 -m pip install -qq torch==$TORCH_VERSION -f https://download.pytorch.org/whl/torch/
 
